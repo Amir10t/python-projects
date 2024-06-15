eada49bd182eed850c353960414f449b5a56a72a
Amir Talebi <amirhasantalebi1400@gmail.com>
2024-06-15 04:20:17 -0700

    Add func_params.py to 02

---
+++ 02_SomeOfNumbers/func_params.py
@@ -0,0 +1,5 @@
+def sum(*args):
+    _sum = 0
+    for arg in args:
+        _sum += int(arg)
+    return _sum
---
+++ 02_SomeOfNumbers/main_params.py
@@ -0,0 +1,6 @@
+nums = input()
+nums = nums.split(" ")
+_sum = 0
+for num in nums:
+    _sum += int(num)
+print(_sum)
