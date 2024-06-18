be992c6a861576576eaa9e44c35c4fe389b4c901
Danger Programmer <assassinscer@gmail.com>
2024-06-18 05:43:43 -0700

    Add main.py calculator

---
+++ main.py
@@ -0,0 +1,17 @@
+x=int(input("Number 1 :"))
+y=int(input("Number 2 :"))
+
+print(f"{x} + {y}")
+print(x+y)
+print("------------------")
+print(f"{x} - {y}")
+print(x-y)
+print("------------------")
+print(f"{x} * {y}")
+print(x*y)
+print("------------------")
+print(f"{x} / {y}")
+print(x/y)
+
+
+
